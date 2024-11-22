99f7db8821041c8a0c33d0b1bbe328c6e5fb6b7f
psithurism <2716570623@qq.com>
2024-11-22 18:58:41 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,13 +4,15 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
+    /** 判断是否为偶数 */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
+        if (n % 2 == 1)
+        {
             return 3 * n + 1;
-        } else {
-            return n * 2;
+        }
+        else
+        {
+            return n / 2;
         }
     }
 
