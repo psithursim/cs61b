52403fee41e66cf69bc727cc5265c6a9556c2ca1
psithurism <2716570623@qq.com>
2024-12-03 21:20:18 +0800

    solove lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(2, 3, 16, 21, 23);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 16 -> 21 -> 529", lst.toString());
+        assertTrue(changed);
+    }
 }
