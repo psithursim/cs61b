a1b799e649365dc753ad5c130141233fe0c0c4fe
psithurism <2716570623@qq.com>
2024-12-22 21:08:22 +0800

    correct proj1ec

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -11,30 +11,30 @@ public class TestArrayDequeEC {
         StringBuilder str = new StringBuilder();
         StudentArrayDeque<Integer> sd = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ad= new ArrayDequeSolution<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             Integer num = StdRandom.uniform(-4,4);
             if (num <= -2) {
                 sd.addLast(num);
                 ad.addLast(num);
                 str.append("addLast(" + num + ")\n");
-                assertEquals("Oh noooo!\nThis is bad:\n" + str + "The last step is wrong",sd.size(), ad.size());
+                assertEquals(str + "\n",sd.size(), ad.size());
             } else if (num <= 0) {
                 sd.addFirst(num);
                 ad.addFirst(num);
                 str.append("addFirst(" + num + ")\n");
-                assertEquals("Oh noooo!\nThis is bad:\n" + str + "The last step is wrong",sd.size(), ad.size());
+                assertEquals(str + "\n",sd.size(), ad.size());
             } else if (num <= 2) {
                 if (ad.isEmpty()) {
                     continue;
                 }
                 str.append("removeFirst()\n");
-                assertEquals("Oh noooo!\nThis is bad:\n" + str,ad.removeFirst(), sd.removeFirst());
+                assertEquals(str + "\n",ad.removeFirst(), sd.removeFirst());
             } else {
                 if (ad.isEmpty()) {
                     continue;
                 }
                 str.append("removeLast()\n");
-                assertEquals("Oh noooo!\nThis is bad:\n" + str + "The last step is wrong",ad.removeLast(), sd.removeLast());
+                assertEquals(str + "\n",ad.removeLast(), sd.removeLast());
             }
         }
     }
