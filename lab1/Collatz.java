445348e1067b40860cc0db6e71ff787f20cfc259
psithurism <2716570623@qq.com>
2024-12-29 14:58:22 +0800

    change lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,8 +5,8 @@ public class Collatz {
     
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
